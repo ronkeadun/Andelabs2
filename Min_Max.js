f559a99daa579adf6fea9d48bd961654aad8145c
ronkeadun <ronkeadun1@yahoo.com>
2017-02-09 11:25:58 +0100

    second commit

--- Min_Max.js
+++ Min_Max.js
@@ -1,3 +1,13 @@
-function Min_Max (n){
+function findMinMax (n){
+	var Arr = [];
+	for (var x in n) {
+		if ( n[x] < n[x + 1]){
 
+			Arr.push(n[x]);
+			Arr.push(n[x + 1]);
+		}
+		
+	}
+
+	return Arr;
 }
